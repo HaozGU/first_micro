8a96849e2626ac71487ac296a9e41d7a6b74a2ef
HaozGU <1298610655@qq.com>
2022-12-03 00:00:02 +0100

    code change

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
 <script async
-src="https://www.googletagmanager.com/gtag/js?id=YOUR_GA_CODE"></script>
+src="https://www.googletagmanager.com/gtag/js?id=UA-250950046-1"></script>
 <script>
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
- gtag('config', ' YOUR_GA_CODE');
+ gtag('config', 'UA-250950046-1');
 </script>
  """
  return prefix_google + "Hello World"
