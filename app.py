33cbc3b7dd52012cad9653c12936065b8bc909c2
HaozGU <1298610655@qq.com>
2022-12-02 23:55:31 +0100

    add new code in app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 def hello_world():
-    prefix_google = """
-    <!-- Google tag (gtag.js) -->
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
 <script async
-src="https://www.googletagmanager.com/gtag/js?id=UA-250950046-1"></script>
+src="https://www.googletagmanager.com/gtag/js?id=YOUR_GA_CODE"></script>
 <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-250950046-1');
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' YOUR_GA_CODE');
 </script>
  """
-    return prefix_google + "Hello World"
+ return prefix_google + "Hello World"
 
 from flask import Flask
 
