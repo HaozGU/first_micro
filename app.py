e500e90f17081d53f0990e819095c5e0d6b47b15
HaozGU <1298610655@qq.com>
2022-12-04 00:00:26 +0100

    code change

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello World123"
+    return "Hello World1234"
     
 def hello_world_google_tag():
  prefix_google = """
